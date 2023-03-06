c6787879ea763ec834514457497aa6530119c8e4
mstislav <steve128@yandex.ru>
2023-03-06 15:37:59 +0300

    Добавлено время к диалогам

--- client/src/main/java/guiElements/BubbleInGoing.java
+++ client/src/main/java/guiElements/BubbleInGoing.java
@@ -3,12 +3,16 @@ package guiElements;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 
+import java.util.Date;
+
 public class BubbleInGoing extends Label {
     private final String stylesInGoingLabel = "-fx-background-color: #97daf7;" +
             "-fx-background-radius: 25px;" + "-fx-background-insets: -10;";
 
     public BubbleInGoing(String msg) {
-        this.setText(msg);
+        Date date = new Date();
+        String msgText = String.format("%s \n [%s]", msg, date);
+        this.setText(msgText);
         this.setStyle(stylesInGoingLabel);
         this.setFont(new Font("Arial", 16));
         this.setMaxWidth(350);
--- client/src/main/java/guiElements/BubbleOutGoing.java
+++ client/src/main/java/guiElements/BubbleOutGoing.java
@@ -3,12 +3,16 @@ package guiElements;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 
+import java.util.Date;
+
 public class BubbleOutGoing extends Label {
     private final String stylesOutGoingLabel = "-fx-background-color: #afdec4;" +
             "-fx-background-radius: 25px;" + "-fx-background-insets: -10;";
 
     public BubbleOutGoing(String msg) {
-        this.setText(msg);
+        Date date = new Date();
+        String msgText = String.format("%s \n [%s]", msg, date);
+        this.setText(msgText);
         this.setStyle(stylesOutGoingLabel);
         this.setFont(new Font("Arial", 16));
         this.setMaxWidth(350);
